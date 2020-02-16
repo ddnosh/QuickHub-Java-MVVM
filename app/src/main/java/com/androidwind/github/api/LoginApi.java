3c9827c8c90d5b47de0d7805db1685d2c732ba3c
ddnosh <ddnosh@qq.com>
2020-02-16 10:29:28 +0800

    优化代码

--- app/src/main/java/com/androidwind/github/api/LoginApi.java
+++ app/src/main/java/com/androidwind/github/api/LoginApi.java
@@ -15,19 +15,7 @@ import retrofit2.http.POST;
  */
 public interface LoginApi {
 
-    // @GET("user")
-    // Observable<GithubAuth> login(@Header("Authorization") String authorization);
-
-    // @GET("users/{github_name}")
-    // Observable<GithubUser> getGithubUser(@Path("github_name") String github_name);
-
-    //retrofit访问普通url
-    // @GET
-    // Observable<GithubUser> getGithubUser(@Url String url);
-
     @POST("authorizations")
     @Headers("Accept: application/json")
-    Observable<GithubBasicToken> authorizations(
-            @NonNull @Body AuthRequestModel authRequestModel
-    );
+    Observable<GithubBasicToken> authorizations(@NonNull @Body AuthRequestModel authRequestModel);
 }
